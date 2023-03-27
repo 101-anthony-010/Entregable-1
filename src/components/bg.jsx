015f0cbfdb915f03ab658d5e8ddeaaa1f2b2bc97
antony <gonzalescamposantony@gmail.com>
2023-03-27 17:29:16 -0500

    second commit

--- src/components/bg.jsx
+++ src/components/bg.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bg = ({props}) => {
   return (
-    <div className='fixed top-0 left-0 w-screen h-screen z-0 bg-no-repeat bg-center bg-cover' style={{ backgroundImage: `url(${props})`}}>
+    <div  style={{ backgroundImage: `url("${props}")`}} className='fixed top-0 left-0 w-screen h-screen z-0 bg-no-repeat bg-center bg-cover'>
     </div>
   )
 }
